24f71c66ce080b57d8352c7212605aa0b767af0a
mnaofficialbd <mna.official92@gmail.com>
2024-12-28 22:03:42 +0600

    server setup and DB connect

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,47 @@
-require("dotenv").config();
-const express = require("express");
+import cors from "cors";
+import { config } from "dotenv";
+import express, { json } from "express";
+import { set, connect } from "mongoose";
+
+// const userRoutes = require("./routes/users");
+// const authRoutes = require("./routes/auth");
+
+config();
+
 const app = express();
-const cors = require("cors");
-const connection = require("./db");
-const userRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth");
+const port = process.env.PORT || 8000;
+
+const corsOption = {
+  origin: true,
+};
 
 // database connection
-connection();
+set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.log("MongoDB connection fail");
+  }
+};
 
 // middlewares
-app.use(express.json());
-app.use(cors());
+app.use(json());
+app.use(cors(corsOption));
 
 // routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/users", userRoutes);
 
-const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.get("/", (req, res) => {
+  res.send("Power Hack's Api is working");
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log("Power Hack's Server is running on port" + " " + port);
+});
