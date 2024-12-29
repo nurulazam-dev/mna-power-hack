e6ad3f84ab548713ee697ee0e0197c355774e2a2
mnaofficialbd <mna.official92@gmail.com>
2024-12-29 19:27:02 +0600

    auth routes create and setup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,38 +1,9 @@
 import express from "express";
-import { User } from "../models/user.js";
-import { compare } from "bcrypt";
-import joiFrom from "joi";
+import { login, register } from "../Controllers/authController.js";
 
-const { object, string } = joiFrom;
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const { error } = validate(req.body);
-    if (error)
-      return res.status(400).send({ message: error.details[0].message });
-
-    const user = await User.findOne({ email: req.body.email });
-    if (!user)
-      return res.status(401).send({ message: "Invalid Email or Password" });
-
-    const validPassword = await compare(req.body.password, user.password);
-    if (!validPassword)
-      return res.status(401).send({ message: "Invalid Email or Password" });
-
-    const token = user.generateAuthToken();
-    res.status(200).send({ data: token, message: "logged in successfully" });
-  } catch (error) {
-    res.status(500).send({ message: "Internal Server Error" });
-  }
-});
-
-const validate = (data) => {
-  const schema = object({
-    email: string().email().required().label("Email"),
-    password: string().required().label("Password"),
-  });
-  return schema.validate(data);
-};
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
