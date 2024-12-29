e1817bbdcb77987b767defccc5f4c2e61e3eb9ae
mnaofficialbd <mna.official92@gmail.com>
2024-12-29 19:18:43 +0600

    register auth-controller create

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -11,3 +11,39 @@ const generateToken = (user) => {
     }
   );
 };
+
+export const register = async (req, res) => {
+  const { email, password, name, role } = req.body;
+  try {
+    let user = null;
+
+    if (role == "accountant") {
+      user = await User.findOne({ email });
+    }
+
+    //check if user exist
+    if (user) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    if (role == "accountant") {
+      user = new User({
+        name,
+        email,
+        password: hashPassword,
+        role,
+      });
+    }
+
+    await user.save();
+    res
+      .status(200)
+      .json({ status: true, message: "User successfully created" });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "User created fail" });
+  }
+};
