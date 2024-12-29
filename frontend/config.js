c7f55d6534208af7e0a4adf5d6cd5689018e199c
mnaofficialbd <mna.official92@gmail.com>
2024-12-29 20:25:00 +0600

    Loading component create

---
+++ frontend/config.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL =
+//   "https://power-hack-backend.onrender.com/api/v1";
+export const token = localStorage.getItem("token");
