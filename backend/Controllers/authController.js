429542eb57b6d80179f71bff9d7296cf74d772af
mnaofficialbd <mna.official92@gmail.com>
2024-12-29 19:15:02 +0600

    auth-controller create

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/UserSchema.js";
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "1d",
+    }
+  );
+};
