5333e5e91b350c4bc3225777bca260b1cc667ab7
mnaofficialbd <mna.official92@gmail.com>
2024-12-30 23:37:05 +0600

    updateBill controller create and setup

--- backend/Controllers/billController.js
+++ backend/Controllers/billController.js
@@ -29,6 +29,29 @@ export const createBill = async (req, res, next) => {
       data: newBill,
     });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    res.status(500).json({
+      success: false,
+
+      message: "Bill isn't submitted",
+      message: error.message,
+    });
+  }
+};
+
+// update controller
+export const updateBill = async (req, res, next) => {
+  const { id } = req.params;
+  const bill = await Bill(id, req.body);
+  res.status(200).json({
+    status: true,
+    message: "Bill updated successfully",
+  });
+  try {
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: "Bill isn't updated",
+      error: error.message,
+    });
   }
 };
