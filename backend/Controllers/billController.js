1a130e07b51b9146d3777c787fa8f5c13e3dc1ef
mnaofficialbd <mna.official92@gmail.com>
2024-12-30 23:29:20 +0600

    create-bill controller create and setup

--- backend/Controllers/billController.js
+++ backend/Controllers/billController.js
@@ -1,6 +1,6 @@
 import Bill from "../models/BillSchema.js";
 
-// get all bills
+// get all bills controller
 export const getAllBills = async (req, res) => {
   try {
     const bills = await Bill.find({});
@@ -12,5 +12,23 @@ export const getAllBills = async (req, res) => {
     });
   } catch (error) {
     res.status(404).json({ success: false, message: "Bills not found" });
+  }
+};
+
+// create a bill controller
+export const createBill = async (req, res, next) => {
+  try {
+    const newBill = new Bill(req.body);
+    //   const savedBill = await newBill.save();
+
+    newBill.logger();
+
+    res.status(200).json({
+      success: true,
+      message: "Bill submitted",
+      data: newBill,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
   }
 };
