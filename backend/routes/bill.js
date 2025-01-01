65d639605fb99c52315f1a7c80b4aba6d796989f
mnaofficialbd <mna.official92@gmail.com>
2025-01-01 20:53:41 +0600

    bill routes update

---
+++ backend/routes/bill.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+// import { authenticate, restrict } from "../auth/verifyToken.js";
+import { authenticate } from "../auth/verifyToken.js";
+import {
+  createBill,
+  deleteBill,
+  getAllBills,
+  updateBill,
+} from "../Controllers/billController.js";
+const router = express.Router();
+
+router.get("/", authenticate, getAllBills);
+// router.post("/", authenticate, restrict(["accountant"]), createBill);
+router.post("/", authenticate, createBill);
+// router.put("/:id", authenticate, restrict(["accountant"]), updateBill);
+router.put("/:id", authenticate, updateBill);
+// router.delete("/:id", authenticate, restrict(["admin"]), deleteBill);
+router.delete("/:id", authenticate, deleteBill);
+
+export default router;
