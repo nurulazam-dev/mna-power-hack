d4c41621c8497009a83a6d066ec691008047efbf
mnaofficialbd <mna.official92@gmail.com>
2025-01-01 23:05:15 +0600

    delete-user-controller create

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -19,3 +19,18 @@ export const updateUser = async (req, res) => {
     res.status(500).json({ success: false, message: "Fail to update user" });
   }
 };
+
+// delete_single_User controller
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await User.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted user",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Fail to delete user" });
+  }
+};
