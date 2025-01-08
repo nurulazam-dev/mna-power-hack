d5fbcced105c528a46426837c7d163a4210ba4f2
mnaofficialbd <mna.official92@gmail.com>
2025-01-08 20:57:08 +0600

    dashboard overview component update

--- frontend/src/components/Dashboard/Overview.jsx
+++ frontend/src/components/Dashboard/Overview.jsx
@@ -2,11 +2,18 @@ const Overview = () => {
   return (
     <section className="text-black">
       <h1 className="text-4xl mb-6 font-bold">Welcome to the Dashboard!</h1>
-
+      {/* ======================================= */}
+      <div className="flex justify-center w-full">
+        {/* middle part */}
+        <div className="w-full lg:w-[75%] lg:mr-5 mb-6 min-h-screen border border-green-700"></div>
+        {/* last part */}
+        <div className="w-full lg:w-[25%]  mb-6 min-h-screen border border-red-700"></div>
+      </div>
+      {/* ======================================= */}
       {/* Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-white">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-black">
         {/* Card 1 */}
-        <div className="card rounded w-full bg-green-600 shadow-xl">
+        <div className="card bg-white rounded w-full border shadow-xl">
           <div className="card-body py-5">
             <h2 className="card-title">Total Users</h2>
             <p className="text-4xl">1,234</p>
@@ -14,7 +21,7 @@ const Overview = () => {
         </div>
 
         {/* Card 2 */}
-        <div className="card rounded w-full bg-orange-500 shadow-xl">
+        <div className="card bg-white rounded w-full border shadow-xl">
           <div className="card-body py-5">
             <h2 className="card-title">Billing Officer</h2>
             <p className="text-4xl">567</p>
@@ -22,46 +29,25 @@ const Overview = () => {
         </div>
 
         {/* Card 3 */}
-        <div className="card rounded w-full bg-slate-600 shadow-xl">
+        <div className="card bg-white rounded w-full border shadow-xl">
           <div className="card-body py-5">
             <h2 className="card-title">Accountant</h2>
             <p className="text-4xl">32</p>
           </div>
         </div>
 
-        {/* Card 4 */}
-        <div className="card rounded w-full bg-blue-600 shadow-xl">
-          <div className="card-body py-5">
-            <h2 className="card-title">Admin</h2>
-            <p className="text-4xl">07</p>
-          </div>
-        </div>
         {/* Card 5 */}
-        <div className="card rounded w-full bg-cyan-600 shadow-xl">
+        <div className="card bg-white rounded w-full border shadow-xl">
           <div className="card-body py-5">
             <h2 className="card-title">Total Bills</h2>
             <p className="text-4xl">4,567</p>
           </div>
         </div>
         {/* Card 6 */}
-        <div className="card rounded w-full bg-lime-600 shadow-xl">
+        <div className="card bg-white rounded w-full border shadow-xl">
           <div className="card-body py-5">
             <h2 className="card-title">Total Amount</h2>
             <p className="text-4xl">$ 4,34,567</p>
-          </div>
-        </div>
-        {/* Card 6 */}
-        <div className="card rounded w-full bg-indigo-700 shadow-xl">
-          <div className="card-body py-5">
-            <h2 className="card-title">Paid Amount</h2>
-            <p className="text-4xl">$ 4,22,396</p>
-          </div>
-        </div>
-        {/* Card 8 */}
-        <div className="card rounded w-full bg-red-600 shadow-xl">
-          <div className="card-body py-5">
-            <h2 className="card-title">Unpaid Amount</h2>
-            <p className="text-4xl">$ 10,480</p>
           </div>
         </div>
       </div>
