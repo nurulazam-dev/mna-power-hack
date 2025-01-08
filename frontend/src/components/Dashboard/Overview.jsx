dbe68cd2275c7d1b6a324416a48537e5f82e78a2
mnaofficialbd <mna.official92@gmail.com>
2025-01-08 21:11:44 +0600

    Overview-cards component update

--- frontend/src/components/Dashboard/Overview.jsx
+++ frontend/src/components/Dashboard/Overview.jsx
@@ -1,3 +1,5 @@
+import OverviewCards from "./Overview/OverviewCards";
+
 const Overview = () => {
   return (
     <section className="text-black">
@@ -7,46 +9,7 @@ const Overview = () => {
         {/* middle part */}
         <div className="w-full lg:w-[75%] lg:mr-5 mb-6 min-h-screen border border-green-700">
           {/* Cards */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-black">
-            {/* Card 1 */}
-            <div className="card bg-white rounded w-full border shadow-xl">
-              <div className="card-body py-5">
-                <h2 className="card-title">Total Users</h2>
-                <p className="text-4xl">1,234</p>
-              </div>
-            </div>
-
-            {/* Card 2 */}
-            <div className="card bg-white rounded w-full border shadow-xl">
-              <div className="card-body py-5">
-                <h2 className="card-title">Billing Officer</h2>
-                <p className="text-4xl">567</p>
-              </div>
-            </div>
-
-            {/* Card 3 */}
-            <div className="card bg-white rounded w-full border shadow-xl">
-              <div className="card-body py-5">
-                <h2 className="card-title">Accountant</h2>
-                <p className="text-4xl">32</p>
-              </div>
-            </div>
-
-            {/* Card 5 */}
-            <div className="card bg-white rounded w-full border shadow-xl">
-              <div className="card-body py-5">
-                <h2 className="card-title">Total Bills</h2>
-                <p className="text-4xl">4,567</p>
-              </div>
-            </div>
-            {/* Card 6 */}
-            <div className="card bg-white rounded w-full border shadow-xl">
-              <div className="card-body py-5">
-                <h2 className="card-title">Total Amount</h2>
-                <p className="text-4xl">$ 4,34,567</p>
-              </div>
-            </div>
-          </div>
+          <OverviewCards />
         </div>
         {/* last part */}
         <div className="w-full lg:w-[25%]  mb-6 min-h-screen border border-red-700"></div>
--- frontend/src/components/Dashboard/Overview/OverviewCards.jsx
+++ frontend/src/components/Dashboard/Overview/OverviewCards.jsx
@@ -1,5 +1,46 @@
 const OverviewCards = () => {
-  return <section></section>;
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 text-black">
+      {/* Card 1 */}
+      <div className="card bg-white rounded w-full border shadow-xl">
+        <div className="card-body py-5">
+          <h2 className="card-title">Total Users</h2>
+          <p className="text-4xl">1,234</p>
+        </div>
+      </div>
+
+      {/* Card 2 */}
+      <div className="card bg-white rounded w-full border shadow-xl">
+        <div className="card-body py-5">
+          <h2 className="card-title">Billing Officer</h2>
+          <p className="text-4xl">567</p>
+        </div>
+      </div>
+
+      {/* Card 3 */}
+      <div className="card bg-white rounded w-full border shadow-xl">
+        <div className="card-body py-5">
+          <h2 className="card-title">Accountant</h2>
+          <p className="text-4xl">32</p>
+        </div>
+      </div>
+
+      {/* Card 4 */}
+      <div className="card bg-white rounded w-full border shadow-xl">
+        <div className="card-body py-5">
+          <h2 className="card-title">Total Bills</h2>
+          <p className="text-4xl">4,567</p>
+        </div>
+      </div>
+      {/* Card 5 */}
+      <div className="card bg-white rounded w-full border shadow-xl">
+        <div className="card-body py-5">
+          <h2 className="card-title">Total Amount</h2>
+          <p className="text-4xl">$ 4,34,567</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OverviewCards;
