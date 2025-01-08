bb1a7c129d0fffb9bf4e0b145ae7281dd7d905a4
mnaofficialbd <mna.official92@gmail.com>
2025-01-09 00:58:25 +0600

    overview-cards component update

--- frontend/src/components/Dashboard/Overview/OverviewCards.jsx
+++ frontend/src/components/Dashboard/Overview/OverviewCards.jsx
@@ -1,4 +1,7 @@
 import { FaUsers } from "react-icons/fa";
+import { MdGroupAdd, MdAddChart } from "react-icons/md";
+import { LiaAddressCardSolid } from "react-icons/lia";
+import { FaSackDollar } from "react-icons/fa6";
 
 const OverviewCards = () => {
   return (
@@ -6,44 +9,52 @@ const OverviewCards = () => {
       {/* Card 1 */}
       <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
         <div>
-          <h2 className="card-title font-bold text-indigo-600">Total Users</h2>
+          <h2 className=" font-bold text-indigo-600">Total Users</h2>
           <p className="text-3xl">1,234</p>
         </div>
-        <div className="bg-indigo-600 rounded-full p-1">
-          <FaUsers className="w-14 h-14 text-white" />
+        <div>
+          <FaUsers className="w-14 h-14 text-indigo-600" />
         </div>
       </div>
-
       {/* Card 2 */}
-      <div className="bg-white rounded w-full border shadow-xl">
-        <div className="p-4">
-          <h2 className="card-title font-bold text-indigo-600">
-            Billing Officer
-          </h2>
+      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
+        <div>
+          <h2 className="font-bold text-indigo-600">Billing Officer</h2>
           <p className="text-3xl">567</p>
+        </div>
+        <div>
+          <MdAddChart className="w-14 h-14 text-indigo-600" />
+        </div>
+      </div>
+      {/* Card 3 */}
+      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
+        <div>
+          <h2 className="font-bold text-indigo-600">Accountant</h2>
+          <p className="text-3xl">41</p>
+        </div>
+        <div>
+          <MdGroupAdd className="w-14 h-14 text-indigo-600" />
         </div>
       </div>
 
       {/* Card 3 */}
-      <div className="bg-white rounded w-full border shadow-xl">
-        <div className="p-4">
-          <h2 className="card-title font-bold text-indigo-600">Accountant</h2>
-          <p className="text-3xl">32</p>
-        </div>
-      </div>
-
-      {/* Card 4 */}
-      <div className="bg-white rounded w-full border shadow-xl">
-        <div className="p-4">
-          <h2 className="card-title font-bold text-indigo-600">Total Bills</h2>
+      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
+        <div>
+          <h2 className="font-bold text-indigo-600">Total Bills</h2>
           <p className="text-3xl">4,567</p>
         </div>
+        <div>
+          <LiaAddressCardSolid className="w-14 h-14 text-indigo-600" />
+        </div>
       </div>
-      {/* Card 5 */}
-      <div className="bg-white rounded w-full border shadow-xl">
-        <div className="p-4">
-          <h2 className="card-title text-indigo-600 font-bold">Total Amount</h2>
-          <p className="text-3xl">$ 4,34,567</p>
+      {/* Card 4 */}
+      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
+        <div>
+          <h2 className="font-bold text-indigo-600">Total Amount</h2>
+          <p className="text-[27px]">$434567</p>
+        </div>
+        <div>
+          <FaSackDollar className="w-14 h-14 text-indigo-600" />
         </div>
       </div>
     </section>
