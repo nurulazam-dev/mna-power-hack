1cd0f1a246ba56374226a099dcb3894dd18236a1
mnaofficialbd <mna.official92@gmail.com>
2025-01-09 00:13:48 +0600

    overview top-bill-holder component update

--- frontend/src/components/Dashboard/Overview/TopBillHolder.jsx
+++ frontend/src/components/Dashboard/Overview/TopBillHolder.jsx
@@ -4,7 +4,7 @@ import { FaPhone } from "react-icons/fa6";
 
 const TopBillHolder = () => {
   return (
-    <section className="overflow-x-auto">
+    <section className="shadow-xl bg-white rounded-md mb-4">
       <h2 className="font-bold text-2xl text-violet-600 mb-2">
         Top Bill Holder
       </h2>
@@ -36,76 +36,81 @@ const TopBillHolder = () => {
             </th>
           </tr>
           {/* row 2 */}
-          <tr>
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
+          <tr className="border-none">
+            <td className="p-0">
+              <div className="flex items-center gap-2">
+                <div className="avatar p-1">
                   <div className="mask rounded-full mask-squircle h-12 w-12">
                     <img
                       src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-                      alt="Avatar Tailwind CSS Component"
+                      alt=""
                     />
                   </div>
                 </div>
                 <div>
                   <div className="font-bold">Brice Swyre</div>
-                  <div className="text-sm opacity-50">China</div>
+                  <div className="text-sm opacity-50">Oxygen, Ctg.</div>
                 </div>
               </div>
             </td>
 
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
+            <th className="flex justify-center items-center">
+              <FaUserCircle className="h-6 w-6 text-blue-600 mr-2" />
+              <MdEmail className="h-6 w-6 text-indigo-600 mr-2" />
+              <FaPhone className="h-5 w-5 text-green-600" />
             </th>
           </tr>
           {/* row 3 */}
-          <tr>
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
+          <tr className="border-none">
+            <td className="p-0">
+              <div className="flex items-center gap-2">
+                <div className="avatar p-1">
                   <div className="mask rounded-full mask-squircle h-12 w-12">
                     <img
                       src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-                      alt="Avatar Tailwind CSS Component"
+                      alt=""
                     />
                   </div>
                 </div>
                 <div>
                   <div className="font-bold">Marjy Ferencz</div>
-                  <div className="text-sm opacity-50">Russia</div>
+                  <div className="text-sm opacity-50">Hathazary, Ctg.</div>
                 </div>
               </div>
             </td>
 
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
+            <th className="flex justify-center items-center">
+              <FaUserCircle className="h-6 w-6 text-blue-600 mr-2" />
+              <MdEmail className="h-6 w-6 text-indigo-600 mr-2" />
+              <FaPhone className="h-5 w-5 text-green-600" />
             </th>
           </tr>
           {/* row 4 */}
-          <tr>
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
+          <tr className="border-none">
+            <td className="p-0">
+              <div className="flex items-center gap-2">
+                <div className="avatar p-1">
                   <div className="mask rounded-full mask-squircle h-12 w-12">
                     <img
                       src="https://img.daisyui.com/images/profile/demo/5@94.webp"
-                      alt="Avatar Tailwind CSS Component"
+                      alt=""
                     />
                   </div>
                 </div>
                 <div>
                   <div className="font-bold">Yancy Tear</div>
-                  <div className="text-sm opacity-50">Brazil</div>
+                  <div className="text-sm opacity-50">Raozan, Ctg.</div>
                 </div>
               </div>
             </td>
 
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
+            <th className="flex justify-center items-center">
+              <FaUserCircle className="h-6 w-6 text-blue-600 mr-2" />
+              <MdEmail className="h-6 w-6 text-indigo-600 mr-2" />
+              <FaPhone className="h-5 w-5 text-green-600" />
             </th>
           </tr>
         </tbody>
-        {/* foot */}
       </table>
     </section>
   );
