89e3af3114607e390ab70944f5e9b93ad9091771
mnaofficialbd <mna.official92@gmail.com>
2025-01-08 22:15:57 +0600

    My-day-picker component update

--- frontend/src/components/Dashboard/Overview/MyDayPicker.jsx
+++ frontend/src/components/Dashboard/Overview/MyDayPicker.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 
 const MyDayPicker = () => {
-  const [selected, setSelected] = useState();
   return (
     <section className="shadow-xl pb-2 mb-5">
-      <DayPicker mode="single" selected={selected} onSelect={setSelected} />
+      <DayPicker mode="single" selected={[new Date()]} />
     </section>
   );
 };
