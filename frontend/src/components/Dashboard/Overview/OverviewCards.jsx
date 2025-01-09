27e1e76b3515e2c6d1f7aa71ec177a789dfe8abf
mnaofficialbd <mna.official92@gmail.com>
2025-01-09 23:09:35 +0600

    overview OverviewCards component update

--- frontend/src/components/Dashboard/Overview/OverviewCards.jsx
+++ frontend/src/components/Dashboard/Overview/OverviewCards.jsx
@@ -1,5 +1,5 @@
 import { FaUsers } from "react-icons/fa";
-import { MdGroupAdd, MdAddChart } from "react-icons/md";
+
 import { LiaAddressCardSolid } from "react-icons/lia";
 import { FaSackDollar } from "react-icons/fa6";
 
@@ -19,27 +19,6 @@ const OverviewCards = () => {
       {/* Card 2 */}
       <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
         <div>
-          <h2 className="font-bold text-indigo-600">Billing Officer</h2>
-          <p className="text-3xl">567</p>
-        </div>
-        <div>
-          <MdAddChart className="w-14 h-14 text-indigo-600" />
-        </div>
-      </div>
-      {/* Card 3 */}
-      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
-        <div>
-          <h2 className="font-bold text-indigo-600">Accountant</h2>
-          <p className="text-3xl">41</p>
-        </div>
-        <div>
-          <MdGroupAdd className="w-14 h-14 text-indigo-600" />
-        </div>
-      </div>
-
-      {/* Card 3 */}
-      <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
-        <div>
           <h2 className="font-bold text-indigo-600">Total Bills</h2>
           <p className="text-3xl">4,567</p>
         </div>
@@ -47,7 +26,7 @@ const OverviewCards = () => {
           <LiaAddressCardSolid className="w-14 h-14 text-indigo-600" />
         </div>
       </div>
-      {/* Card 4 */}
+      {/* Card 3 */}
       <div className="bg-white rounded w-full border shadow-xl flex justify-between items-center p-4">
         <div>
           <h2 className="font-bold text-indigo-600">Total Amount</h2>
