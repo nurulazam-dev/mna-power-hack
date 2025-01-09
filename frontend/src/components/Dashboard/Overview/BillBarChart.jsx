e58de82411aaa28b892ca6e47341c794e1545e06
mnaofficialbd <mna.official92@gmail.com>
2025-01-09 22:59:49 +0600

    overview BillBarChart component update

--- frontend/src/components/Dashboard/Overview/BillBarChart.jsx
+++ frontend/src/components/Dashboard/Overview/BillBarChart.jsx
@@ -20,25 +20,32 @@ ChartJS.register(
 
 const BillBarChart = () => {
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: [
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+      "January",
+    ],
     datasets: [
       {
         label: "Total Bills Amount",
-        data: [10000, 35000, 20000, 37000, 30000, 34000], // Example data for total bills
+        data: [41000, 35000, 20000, 37000, 30000, 34000],
         backgroundColor: "rgba(75, 192, 192, 0.6)",
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
       },
       {
         label: "Paid Amount",
-        data: [8000, 22000, 18000, 24000, 28000, 12000], // Example data for paid amounts
+        data: [36000, 22000, 18000, 24000, 28000, 12000],
         backgroundColor: "rgba(54, 162, 235, 0.6)",
         borderColor: "rgba(54, 162, 235, 1)",
         borderWidth: 1,
       },
       {
         label: "Unpaid Amount",
-        data: [2000, 13000, 2000, 12000, 2000, 22000], // Example data for unpaid amounts
+        data: [5000, 13000, 2000, 12000, 2000, 22000],
         backgroundColor: "rgba(255, 99, 132, 0.6)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
@@ -80,7 +87,7 @@ const BillBarChart = () => {
   };
 
   return (
-    <section className="p-2  mt-5 border bg-white shadow-lg">
+    <section className="px-2  my-5 border bg-white shadow-lg">
       <Bar className="mt-5" data={data} options={options} />
     </section>
   );
