1133a5f5fae31b7a1b79d77e50064d30de5b6ab4
mnaofficialbd <mna.official92@gmail.com>
2025-01-16 22:37:43 +0600

    View-Bill-Modal update

--- frontend/src/components/Billings/ViewBillModal.jsx
+++ frontend/src/components/Billings/ViewBillModal.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 
-const ViewBillModal = () => {
-  return <div>View Bill Modal</div>;
+const ViewBillModal = ({ bill }) => {
+  if (!bill) return null;
+
+  return (
+    <>
+      <input type="checkbox" id="bill-view-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative max-w-xl">
+          <label
+            htmlFor="bill-view-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+
+          <h3 className="text-lg font-bold mb-4 text-center">Bill Details</h3>
+          <div className="space-y-3">
+            <p className="text-sm">
+              <strong>Billing ID:</strong> {bill?.id}
+            </p>
+            <p className="text-sm">
+              <strong>Billing Holder Name:</strong> {bill?.billingHolder}
+            </p>
+            <p className="text-sm">
+              <strong>Phone:</strong> {bill?.phone}
+            </p>
+            <p className="text-sm">
+              <strong>Paid Amount:</strong> $ {bill?.amount}
+            </p>
+            <p className="text-sm">
+              <strong>Paid Date:</strong> {bill?.date}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ViewBillModal;
