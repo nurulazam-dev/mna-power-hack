9e12e99e389e72d1dbd87a7d5ea54180ca73762b
mnaofficialbd <mna.official92@gmail.com>
2025-01-20 22:45:45 +0600

    delete-bill modal update

--- frontend/src/components/Billings/DeleteBillModal.jsx
+++ frontend/src/components/Billings/DeleteBillModal.jsx
@@ -24,10 +24,7 @@ const DeleteBillModal = ({ bill, onDelete }) => {
     try {
       const response = await fetch(`${BASE_URL}/bills/${bill._id}`, {
         method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
+        
       });
 
       if (!response.ok) {
