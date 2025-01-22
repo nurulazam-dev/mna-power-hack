9e4e0323fa8e4626a3b151b0e5603267bae1a50d
mnaofficialbd <mna.official92@gmail.com>
2025-01-22 13:29:04 +0600

    homeCopy component update

--- frontend/src/Pages/HomeCopy.jsx
+++ frontend/src/Pages/HomeCopy.jsx
@@ -1,20 +1,6 @@
 const HomeCopy = () => {
   return (
     <div className="bg-gray-50 text-gray-800  ">
-      <section className="relative bg-violet-600 text-white py-10 px-5 text-center">
-        <div className="mx-auto">
-          <h1 className="text-2xl md:text-4xl font-bold mb-2">
-            Power Hack Simplified
-          </h1>
-          <p className="text-lg md:text-xl mb-2">
-            Manage your bills and power consumption with ease. Your ultimate
-            power management tool.
-          </p>
-          <button className="bg-white text-violet-600 font-bold py-3 px-6 rounded-lg shadow-md hover:bg-gray-200 transition">
-            Get Started
-          </button>
-        </div>
-      </section>
       <section className="py-5 px-10">
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-2 text-violet-600">
@@ -54,7 +40,20 @@ const HomeCopy = () => {
           </div>
         </div>
       </section>
-
+      <section className="relative bg-violet-600 text-white py-10 px-5 text-center">
+        <div className="mx-auto">
+          <h1 className="text-2xl md:text-4xl font-bold mb-2">
+            Power Hack Simplified
+          </h1>
+          <p className="text-lg md:text-xl mb-2">
+            Manage your bills and power consumption with ease. Your ultimate
+            power management tool.
+          </p>
+          <button className="bg-white text-violet-600 font-bold py-3 px-6 rounded-lg shadow-md hover:bg-gray-200 transition">
+            Get Started
+          </button>
+        </div>
+      </section>
       <section className="bg-gray-100 py-5 px-10 mb-4">
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-2 text-violet-600">
