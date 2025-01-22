b5e1fe893c3321a91aa4d177084a81f5a8f388f8
mnaofficialbd <mna.official92@gmail.com>
2025-01-22 12:40:22 +0600

    summary component update

--- frontend/src/components/Dashboard/Overview/Summary.jsx
+++ frontend/src/components/Dashboard/Overview/Summary.jsx
@@ -52,37 +52,79 @@ const Summary = () => {
   return (
     <div className="">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="bg-white px-5 shadow-md rounded-lg">
-          <h2 className="text-2xl font-bold mb-2 text-violet-600 text-center">
+        <div className="bg-white px-5 pb-2 shadow-md rounded">
+          <h2 className="text-2xl font-bold mb-1 text-slate-600 text-center">
             Bills Summary:
           </h2>
-          <ul className="text-lg text-gray-700">
-            <li>Total Bills: {bills?.length}</li>
-            <li>
-              Total Paid Bills:{" "}
-              {bills.filter((bill) => bill.status === "Paid")?.length}
-            </li>
-            <li>
-              Total Unpaid Bills:{" "}
-              {bills.filter((bill) => bill.status === "Unpaid")?.length}
-            </li>
-            <li>Total Paid Amount: ${totalPaidAmount?.toFixed(2)}</li>
-            <li>Total Unpaid Amount: ${totalUnpaidAmount?.toFixed(2)}</li>
-          </ul>
+          <table className="table-auto text-[14px] text-gray-600 w-full border-collapse border border-gray-300">
+            <tbody>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Total Bills:</td>
+                <td className="px-4 py-1 text-violet-600">{bills?.length}</td>
+              </tr>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Total Paid Bills:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  {bills.filter((bill) => bill.status === "Paid")?.length}
+                </td>
+              </tr>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Total Unpaid Bills:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  {bills.filter((bill) => bill.status === "Unpaid")?.length}
+                </td>
+              </tr>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Total Paid Amount:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  $ {totalPaidAmount?.toFixed(2)}
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4 py-1 font-medium">Total Unpaid Amount:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  $ {totalUnpaidAmount?.toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
         {/* Users Summary */}
-        <div className="bg-white p-5 shadow-lg rounded-lg">
-          <h2 className="text-2xl font-bold mb-3 text-black">Users Summary</h2>
-          <ul className="text-lg text-gray-700">
-            <li>Total Users: {users?.length}</li>
-            <li>
-              Active Users: {users.filter((user) => user.isActive)?.length}
-            </li>
-            <li>
-              Inactive Users: {users.filter((user) => !user.isActive)?.length}
-            </li>
-          </ul>
+        <div className="bg-white px-5 pb-2 shadow-md rounded">
+          <h2 className="text-2xl font-bold mb-1 text-slate-600 text-center">
+            Users Summary:
+          </h2>
+
+          <table className="table-auto text-[14px] text-gray-600 w-full border-collapse border border-gray-300">
+            <tbody>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Total Users:</td>
+                <td className="px-4 py-1 text-violet-600">{users?.length}</td>
+              </tr>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Billing Officer:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  {
+                    users.filter((user) => user?.role === "billingOfficer")
+                      ?.length
+                  }
+                </td>
+              </tr>
+              <tr className="border-b border-gray-300">
+                <td className="px-4 py-1 font-medium">Accountant:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  {users.filter((user) => user?.role === "accountant")?.length}
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4 py-1 font-medium">Admin:</td>
+                <td className="px-4 py-1 text-violet-600">
+                  {users.filter((user) => user?.role === "admin")?.length}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
