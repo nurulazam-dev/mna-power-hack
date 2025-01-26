615ed07e72f38342cfac650f32f9ee456553b514
mnaofficialbd <mna.official92@gmail.com>
2025-01-27 01:16:22 +0600

    OverviewUserBanner update

--- frontend/src/components/Dashboard/Overview/OverviewUserBanner.jsx
+++ frontend/src/components/Dashboard/Overview/OverviewUserBanner.jsx
@@ -10,16 +10,16 @@ const OverviewUserBanner = () => {
     <section className="bg-gradient-to-r from-violet-800 to-violet-300 text-white rounded-md mb-6">
       <div className="lg:flex justify-between items-center p-2 pb-0">
         {/* info part */}
-        <div className="px-4">
-          <h2 className="font-bold text-2xl mb-2">
+        <div className="px-4 lg:pb-0 pb-3">
+          <h2 className="font-bold lg:text-2xl text-xl mb-2">
             Hello! {user?.name}{" "}
             <span className="text-sm font-semibold">({role})</span>
           </h2>
-          <p className="opacity-85 font-thin text-[15px]">
+          <p className="opacity-85 font-thin text-[14px] lg:text-[15px]">
             Dive into your personalized dashboard for seamless management and
             insights. Thanks for being part of our team!
           </p>
-          <button className="bg-black hover:bg-white py-[7px] rounded font-normal text-[17px] mt-4 mb-2 text-white hover:text-black flex justify-center items-center px-5">
+          <button className="bg-black hover:bg-white py-[5px] lg:py-[7px] rounded font-normal text-[15px] lg:text-[17px] mt-4 lg:mb-2 text-white hover:text-black flex justify-center items-center px-5">
             Profile <MdArrowOutward className="h-5 w-5 ml-1" />
           </button>
         </div>
