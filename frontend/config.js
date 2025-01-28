ab3f74f71468cf024be5388380a5aead9bd2a508
mnaofficialbd <mna.official92@gmail.com>
2025-01-28 22:52:11 +0600

    config update update

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "https://mna-power-hack-backend.onrender.com/api/v1";
+export const BASE_URL = "https://mna-power-hack-backend.onrender.com";
 export const token = localStorage.getItem("token");
